eb9498c15fee7e4e5e1e3cf0f696aea32e3994f2
Victor Deryagin <vderyagin@gmail.com>
2014-10-29 13:23:28 +0200

    Definition of DotFile type.

--- dotfile/dotfile.go
+++ dotfile/dotfile.go
@@ -1,1 +1,10 @@
 package dotfile
+
+// DotFile type represents a single dotfile, defined by its storage and
+// linking location. If dotfile is stored, its StoredLocation corresponds to a
+// file within dotfiles repository and OriginalLocation - to symlink in user
+// home directory where system expects original file to be.
+type DotFile struct {
+	OriginalLocation string
+	StoredLocation   string
+}
