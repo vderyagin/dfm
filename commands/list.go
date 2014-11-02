0b2ba0e204217c02e30afdfb4610b1df877dd384
Victor Deryagin <vderyagin@gmail.com>
2014-11-02 13:30:09 +0200

    Primitive implementation of dotfile listing.

--- commands/list.go
+++ commands/list.go
@@ -1,9 +1,18 @@
 package commands
 
 import (
+	"fmt"
+
 	"github.com/codegangsta/cli"
+	"github.com/vderyagin/dfm/repo"
 )
 
 // List displays a list of stored dotfiles.
 func List(c *cli.Context) {
+	store := c.GlobalString("store")
+	home := c.GlobalString("home")
+
+	for _, df := range repo.New(store, home).StoredDotFiles() {
+		fmt.Println(df)
+	}
 }
