29ae068743e190703168b4968841789d79a270b8
Victor Deryagin <vderyagin@gmail.com>
2014-11-03 10:07:10 +0200

    Refactor dotfile tests.

--- dotfile/dotfile_test.go
+++ dotfile/dotfile_test.go
@@ -2,6 +2,7 @@ package dotfile_test
 
 import (
 	"io/ioutil"
+	"log"
 	"os"
 	"path/filepath"
 
@@ -16,54 +17,66 @@ var _ = Describe("Dotfile", func() {
 	Describe("IsStored", func() {
 		ExecuteEachInTempDir()
 
-		It("Returns true if file is properly stored", func() {
-			stored, _ := filepath.Abs("foo")
-			orig, _ := filepath.Abs(".foo")
-			ioutil.WriteFile(stored, []byte{}, 0777)
-			os.Symlink(stored, orig)
+		stored := func() string {
+			s, err := filepath.Abs("foo")
+			if err != nil {
+				log.Fatal(err)
+			}
+			return s
+		}
 
-			Expect(New(stored, orig).IsStored()).To(BeTrue())
+		orig := func() string {
+			o, err := filepath.Abs(".foo")
+			if err != nil {
+				log.Fatal(err)
+			}
+			return o
+		}
+
+		df := func() *DotFile {
+			return New(stored(), orig())
+		}
+
+		It("Returns true if file is properly stored", func() {
+			ioutil.WriteFile(stored(), []byte{}, 0777)
+			os.Symlink(stored(), orig())
+
+			Expect(df().IsStored()).To(BeTrue())
 		})
 
 		It("Returns false if original file location is empty", func() {
-			stored, _ := filepath.Abs("foo")
-			orig, _ := filepath.Abs(".foo")
-			ioutil.WriteFile(stored, []byte{}, 0777)
+			ioutil.WriteFile(stored(), []byte{}, 0777)
 
-			Expect(New(stored, orig).IsStored()).To(BeFalse())
+			Expect(df().IsStored()).To(BeFalse())
 		})
 
 		It("Returns false if stored file location is empty", func() {
-			stored, _ := filepath.Abs("foo")
-			orig, _ := filepath.Abs(".foo")
+			ioutil.WriteFile(stored(), []byte{}, 0777)
+			os.Symlink(stored(), orig())
+			os.Remove(stored())
 
-			Expect(New(stored, orig).IsStored()).To(BeFalse())
+			Expect(df().IsStored()).To(BeFalse())
 		})
 
 		It("Returns false if original file is not a symlink", func() {
-			stored, _ := filepath.Abs("foo")
-			orig, _ := filepath.Abs(".foo")
-			ioutil.WriteFile(stored, []byte{}, 0777)
-			ioutil.WriteFile(orig, []byte{}, 0777)
+			ioutil.WriteFile(stored(), []byte{}, 0777)
+			ioutil.WriteFile(orig(), []byte{}, 0777)
 
-			Expect(New(stored, orig).IsStored()).To(BeFalse())
+			Expect(df().IsStored()).To(BeFalse())
 		})
 
 		It("Returns false if stored file is not a regular file", func() {
-			stored, _ := filepath.Abs("foo")
-			orig, _ := filepath.Abs(".foo")
-			os.MkdirAll(stored, 0777)
+			os.MkdirAll(stored(), 0777)
+			os.Symlink(stored(), orig())
 
-			Expect(New(stored, orig).IsStored()).To(BeFalse())
+			Expect(df().IsStored()).To(BeFalse())
 		})
 
 		It("Returns false both files are not linked properly", func() {
-			stored, _ := filepath.Abs("foo")
-			orig, _ := filepath.Abs(".foo")
-			ioutil.WriteFile(stored, []byte{}, 0777)
-			os.Symlink("/wrong/location", orig)
+			ioutil.WriteFile(stored(), []byte{}, 0777)
+			os.Symlink("/wrong/location", orig())
 
-			Expect(New(stored, orig).IsStored()).To(BeFalse())
+			Expect(df().IsStored()).To(BeFalse())
 		})
 	})
 })
