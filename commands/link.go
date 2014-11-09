51f805df652a5d3264f5a2f6c19b6d4f8be40735
Victor Deryagin <vderyagin@gmail.com>
2014-11-09 07:02:12 +0200

    link command: do now show "skipped" messages.

--- commands/link.go
+++ commands/link.go
@@ -6,11 +6,13 @@ import "github.com/codegangsta/cli"
 // directory.
 func Link(c *cli.Context) {
 	for _, df := range Repo(c).StoredDotFiles() {
+		if df.IsLinked() {
+			continue
+		}
+
 		logger := Logger(c, df)
 
-		if df.IsLinked() {
-			logger.Skip("skipped linking", "is already linked")
-		} else if err := df.Link(); err == nil {
+		if err := df.Link(); err == nil {
 			logger.Success("linked")
 		} else {
 			logger.Fail("failed to link", err.Error())
