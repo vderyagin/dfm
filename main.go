148d88b551341f9726572a98196264c9471b0c47
Victor Deryagin <vderyagin@gmail.com>
2014-11-10 05:51:54 +0200

    Extract flags and commands into variables.

--- main.go
+++ main.go
@@ -20,6 +20,53 @@ func homeDir() string {
 	return usr.HomeDir
 }
 
+var appFlags = []cli.Flag{
+	cli.StringFlag{
+		Name:   "home",
+		Value:  homeDir(),
+		Usage:  "Home directory",
+		EnvVar: "DOTFILES_HOME_DIR",
+	},
+	cli.StringFlag{
+		Name:   "store",
+		Value:  filepath.Join(homeDir(), "dotfiles-test"),
+		Usage:  "directory files will be stored in",
+		EnvVar: "DOTFILES_STORE_DIR",
+	},
+}
+
+var appCommands = []cli.Command{
+	{
+		Name:      "list",
+		ShortName: "l",
+		Usage:     "List stored dotfiles",
+		Action:    commands.List,
+	},
+	{
+		Name:      "store",
+		ShortName: "s",
+		Usage:     "Put file into store",
+		Action:    commands.Store,
+	},
+	{
+		Name:      "restore",
+		ShortName: "r",
+		Usage:     "Move file to its original location",
+		Action:    commands.Restore,
+	},
+	{
+		Name:   "link",
+		Usage:  "Link all stored files to their original locations",
+		Action: commands.Link,
+	},
+	{
+		Name:      "delete",
+		ShortName: "d",
+		Usage:     "Delete given files from home and store",
+		Action:    commands.Delete,
+	},
+}
+
 func main() {
 	app := cli.NewApp()
 
@@ -27,53 +74,8 @@ func main() {
 	app.Name = "dfm"
 	app.Usage = "Dotfile manager"
 	app.Version = "0.0.1"
-
-	app.Flags = []cli.Flag{
-		cli.StringFlag{
-			Name:   "home",
-			Value:  homeDir(),
-			Usage:  "Home directory",
-			EnvVar: "DOTFILES_HOME_DIR",
-		},
-		cli.StringFlag{
-			Name:   "store",
-			Value:  filepath.Join(homeDir(), "dotfiles-test"),
-			Usage:  "directory files will be stored in",
-			EnvVar: "DOTFILES_STORE_DIR",
-		},
-	}
-
-	app.Commands = []cli.Command{
-		{
-			Name:      "list",
-			ShortName: "l",
-			Usage:     "List stored dotfiles",
-			Action:    commands.List,
-		},
-		{
-			Name:      "store",
-			ShortName: "s",
-			Usage:     "Put file into store",
-			Action:    commands.Store,
-		},
-		{
-			Name:      "restore",
-			ShortName: "r",
-			Usage:     "Move file to its original location",
-			Action:    commands.Restore,
-		},
-		{
-			Name:   "link",
-			Usage:  "Link all stored files to their original locations",
-			Action: commands.Link,
-		},
-		{
-			Name:      "delete",
-			ShortName: "d",
-			Usage:     "Delete given files from home and store",
-			Action:    commands.Delete,
-		},
-	}
+	app.Flags = appFlags
+	app.Commands = appCommands
 
 	app.Run(os.Args)
 }
