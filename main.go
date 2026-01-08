88c375910d9ac7bbc036cbf01afc8fac4c6d3c36
Victor Deryagin <vderyagin@gmail.com>
2026-01-08 21:29:22 +0200

    v0.3.0

--- main.go
+++ main.go
@@ -94,7 +94,7 @@ func main() {
 	app.Author = "Victor Deryagin <vderyagin@gmail.com>"
 	app.Name = "dfm"
 	app.Usage = "Dotfile manager"
-	app.Version = "0.2.2"
+	app.Version = "0.3.0"
 	app.Flags = appFlags
 	app.Commands = appCommands
 
